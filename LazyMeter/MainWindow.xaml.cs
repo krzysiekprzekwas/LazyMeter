7605ae28aac08debccca7b96d37357c6125bf997
Krynio <przekwaskrzysiek@gmail.com>
2018-02-15 19:51:09 +0100

    Changed app detection to UI Automation

--- LazyMeter/MainWindow.xaml.cs
+++ LazyMeter/MainWindow.xaml.cs
@@ -14,6 +14,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
+using System.Windows.Automation;
 
 namespace LazyMeter
 {
@@ -26,13 +28,48 @@ namespace LazyMeter
         {
             InitializeComponent();
 
-            var processes = Process.GetProcesses();
-            var uniqueProcesses = processes.DistinctBy(x => x.ProcessName);
 
-            foreach (Process p in uniqueProcesses)
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(1);
+            timer.Tick += timer_Tick;
+            timer.Start();
+
+            
+        }
+
+        void timer_Tick(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            
+            ShowApplicationNames();
+        }
+
+        private void ShowApplicationNames()
+        {
+            AutomationElement rootElement = AutomationElement.RootElement;
+            var children = GetChildren(rootElement);
+
+
+            var apps = children.Where(x => !string.IsNullOrWhiteSpace(x.Current.Name)).Select(x=>x);
+
+            foreach (AutomationElement elemnt in apps)
             {
-                    listBox1.Items.Add(p.ProcessName + " - " + p.MainWindowTitle);
+                
+                var listboxitem = new ListBoxItem();
+                listboxitem.Content = elemnt.Current.Name + " - " + elemnt.Current.ProcessId;
+                listBox1.Items.Add(listboxitem);
+                
             }
+            
+        }
+        
+        public List<AutomationElement> GetChildren(AutomationElement parent)
+        {
+            System.Windows.Automation.Condition findCondition = new PropertyCondition(AutomationElement.IsControlElementProperty, true);
+            AutomationElementCollection children = parent.FindAll(TreeScope.Children, findCondition);
+            AutomationElement[] elementArray = new AutomationElement[children.Count];
+            children.CopyTo(elementArray, 0);
+            return elementArray.ToList();
         }
     }
 }
